ec91e07a4bb05701fe0da837fbec2626ac239d97
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-05-08 00:19:17 -0400

    added sample lists on page

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,9 +1,29 @@
-
 import React from 'react'
+import PlaceDetails from './PlaceDetails/PlaceDetails';
+// import { useState } from 'react';
+import {Grid } from '@material-ui/core';
+import { useStyles } from '../Utils/globalStyle';
 
 const List = () => {
+  const places = [
+    {name: "List1 "},
+    {name: "List2 "},
+    {name: "List3 "},
+    {name: "List4 "},
+    {name: "List5 "},
+    {name: "List6"},
+    {name: "List7 "},
+]
+  const classes = useStyles();
+    // const [type, setType] = useState('restaurants');
   return (
-    <h2>list</h2>
-  )
+    <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+                <Grid item key={i} xs={12} >
+                    <PlaceDetails place = {place}/>
+                </Grid>
+            ))}
+    </Grid>
+)
 }
 export default List;
