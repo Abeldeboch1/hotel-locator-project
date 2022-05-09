acb84e7d4cc3026b6527fd4c6679d737b124100c
Abeldeboch1 <abeldeboch1@gmail.com>
2022-05-09 15:54:13 -0600

    MapList component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
 import Header from './components/Header';
 import GlobalStyle from './globalStyle';
-import { useEffect, useState } from 'react';
-import List from './components/list';
-import Map from './components/Map';
+// import { useEffect, useState } from 'react';
+// import List from './components/List';
+// import Map from './components/Map';
+import MapList from './components/MapList';
 
 const App = () => {
-  const [places, setPlaces] = useState([])
-  const [coordinates, setCoordinates] = useState({ lat: -3.745,  lng: -38.523});
-  const [bounds, setBounds] = useState({});
+  /////////////////////////////////////////////
+//   const [places, setPlaces] = useState([])
+//   const [coordinates, setCoordinates] = useState({ lat: -3.745,  lng: -38.523});
+//   const [bounds, setBounds] = useState({});
 
 
-  useEffect(() => {
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=Hotels&latitude=37.786882&longitude=-122.399972&limit=50`, {
-      headers: {
-        Authorization: 'Bearer 4qYnQWEe5NDiKlIVY2bDZNwBqWbpyRrscNfGJga7Vo-UZiqWZNfsxs5iTT42jPMg6iTFGV4NYlM6tiwRddSMXh6nnsAyFXwg9SY0rz2uCI3jXE8pW9rYELiMCNhyYnYx'
-      }
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        console.log(data.businesses[0].id)
-        console.log(data.businesses[0].name);
-        console.log(data.region.center.longitude);
-        // setPlaces(data)
-        setPlaces(data.businesses)
-      }
-      )
-      .catch((error) => console.log(error))
-  }, [])
+//   useEffect(() => {
+//     fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=Hotels&latitude=37.786882&longitude=-122.399972&limit=50`, {
+//       headers: {
+//         Authorization: 'Bearer 4qYnQWEe5NDiKlIVY2bDZNwBqWbpyRrscNfGJga7Vo-UZiqWZNfsxs5iTT42jPMg6iTFGV4NYlM6tiwRddSMXh6nnsAyFXwg9SY0rz2uCI3jXE8pW9rYELiMCNhyYnYx'
+//       }
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//         console.log(data.businesses[0].id)
+//         console.log(data.businesses[0].name);
+//         console.log(data.region.center.longitude);
+//         // setPlaces(data)
+//         setPlaces(data.businesses)
+//       }
+//       )
+//       .catch((error) => console.log(error))
+//   }, [])
 
-useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        setCoordinates({
-          lat: latitude,
-          lng: longitude,
-        });
-      }
-    );
-  }, []);
-
+// useEffect(() => {
+//     navigator.geolocation.getCurrentPosition(
+//       ({ coords: { latitude, longitude } }) => {
+//         setCoordinates({
+//           lat: latitude,
+//           lng: longitude,
+//         });
+//       }
+//     );
+//   }, []);
+/////////////////////////////////////
   //  useEffect(() => {
   //   console.log(coordinates, bounds);
   //   // Answer: here -> getPlacesData(bounds?.sw, bounds?.ne).then((data) => {….. where i receive this data
@@ -53,7 +55,8 @@ useEffect(() => {
     <>
       <GlobalStyle />
       <Header />
-      <div >
+      <MapList />
+      {/* <div>
         <div >
           <Map  
            setBounds={setBounds}
@@ -61,11 +64,11 @@ useEffect(() => {
             setCoordinates={setCoordinates}
           />
         </div>
-       li
+       
         <div >
           <List places ={places}/>
         </div>
-      </div>
+      </div> */}
     </>
 
   );
