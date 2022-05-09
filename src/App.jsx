a00067eab0e7e510299aef3f9d7015c99ebb57ec
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-05-09 12:53:24 -0400

    restaurant lists added

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,73 @@
 import Header from './components/Header';
-import styled from 'styled-components';
-import { Grid, CssBaseline } from '@material-ui/core';
 import GlobalStyle from './globalStyle';
 import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import List from './components/list';
 import Map from './components/Map';
-// import Layout from './pages/Layout';
-
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
 
 const App = () => {
-const [posts, setPosts] = useState([])
-const [places, setPlaces] = useState([])
+  const [places, setPlaces] = useState([])
+  const [coordinates, setCoordinates] = useState({ lat: -3.745,  lng: -38.523});
+  const [bounds, setBounds] = useState({});
 
-useEffect( () => {
-fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=Hotels&latitude=37.786882&longitude=-122.399972&limit=50`, {
-  headers: {
-    Authorization: 'Bearer 4qYnQWEe5NDiKlIVY2bDZNwBqWbpyRrscNfGJga7Vo-UZiqWZNfsxs5iTT42jPMg6iTFGV4NYlM6tiwRddSMXh6nnsAyFXwg9SY0rz2uCI3jXE8pW9rYELiMCNhyYnYx'  
-  }
-})
-.then((res) => res.json())
-.then((data) =>{
-  const hotel = data.id
-  setPosts(data);
-  console.log(data.businesses)
-}
 
-)
-.catch((error) => console.log(error))
-}, [])
+  useEffect(() => {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=Hotels&latitude=37.786882&longitude=-122.399972&limit=50`, {
+      headers: {
+        Authorization: 'Bearer 4qYnQWEe5NDiKlIVY2bDZNwBqWbpyRrscNfGJga7Vo-UZiqWZNfsxs5iTT42jPMg6iTFGV4NYlM6tiwRddSMXh6nnsAyFXwg9SY0rz2uCI3jXE8pW9rYELiMCNhyYnYx'
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.businesses[0].id)
+        console.log(data.businesses[0].name);
+        console.log(data.region.center.longitude);
+        // setPlaces(data)
+        setPlaces(data.businesses)
+      }
+      )
+      .catch((error) => console.log(error))
+  }, [])
 
-// console.log(posts)
+useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({
+          lat: latitude,
+          lng: longitude,
+        });
+      }
+    );
+  }, []);
 
+  //  useEffect(() => {
+  //   console.log(coordinates, bounds);
+  //   // Answer: here -> getPlacesData(bounds?.sw, bounds?.ne).then((data) => {….. where i receive this data
+  //   // getPlacesData(bounds.sw, bounds.ne)
+
+  //       setPlaces(data);
+  //     })
+  // }, [coordinates, bounds ]);
 
   return (
-
-      <>
+    <>
       <GlobalStyle />
       <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}>
-       
-        <Grid item xs={12} md={8}>
-          <Map
-            
+      <div >
+        <div >
+          <Map  
+           setBounds={setBounds}
+            coordinates={coordinates}
+            setCoordinates={setCoordinates}
           />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <List
-           
-          />
-        </Grid>
-    
-      </Grid>
+        </div>
+       li
+        <div >
+          <List places ={places}/>
+        </div>
+      </div>
     </>
-      //   <Routes>
-      //   <Route path="/" element={<Layout />}>
-      //     <Route index element={<Posts />} />
-      //     <Route path="add" element={<AddPost />} />
-      //     <Route path="posts/:id" element={<FullPost />} />
-      //     <Route path="*" element={<NotFound />} />
-      //   </Route>
-      // </Routes>
+
   );
 }
 
---
+++ src/Utils/API.js
@@ -0,0 +1,25 @@
+// import axios from 'axios';
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+//  const getPlacesData = async () => {
+//         try {
+//             const {data:{data}} = await axios.get(URL,{
+//                 params: {
+//                     bl_latitude: 11.847676,
+//                     tr_latitude: 12.838442,
+//                     bl_longitude: 109.095887,
+//                     tr_longitude: 109.149359,
+             
+//                 },
+//                 headers: {
+//                   'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+//                   'X-RapidAPI-Key': '7c84f330d5mshaf7112847c53383p1760d0jsnc270d54c50aa'
+//                 }
+//               });
+            
+//             return data;
+//         } catch (error){
+//   console.log(error)
+//         }
+//     }
+// export default getPlacesData;
